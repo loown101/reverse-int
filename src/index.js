57160937ca161b8ced53a19467ff91099638c456
loown101 <loown101@gmail.com>
2021-03-01 22:51:02 +0300

    init: start reverse-int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        n=n*(-1);
+    }
+    n = n + '';
+    let i = 0;
+    let result = '';
+    while (i < n.length) {
+    result = n[i] + result;
+    i = i + 1;
+    }
+    return +result;
 }
